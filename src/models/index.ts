caedbd3f52193f5fbeb9a7c6027caaf7710351f8
sooyoungkang <kang.sooyoung@hotmail.com>
2022-11-28 15:29:42 +0900

    Add Data and Reponse models

---
+++ src/models/index.ts
@@ -0,0 +1,12 @@
+/**
+ * Data Models
+ */
+export type { Artist } from "./data/artist";
+export type { ComicRankItem } from "./data/comic";
+/**
+ * Response Models
+ */
+export type {
+  ComicRankApiSuccessResponse,
+  ComicRankApiFailResponse,
+} from "./response/comicResponse";
